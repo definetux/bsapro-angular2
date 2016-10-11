6f1961b9894a41b3b09a890bda12ffe3360fb37c
Eduard Dolynskyi <eduard.dolinskyi@binary-studio.com>
2016-10-11 23:00:56 +0300

    fix server file and add express to package

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function response(req, res) {
-	res.sendFile(path.join(__dirname, 'stc/index.html'));
-});
+	res.sendFile(path.join(__dirname, 'src/index.html'));
+ });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
