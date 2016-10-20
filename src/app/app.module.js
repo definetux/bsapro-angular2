dcd4e46acb6a44a351044489de56c2280241c829
Eduard Dolynskyi <eduard.dolinskyi@binary-studio.com>
2016-10-20 04:37:10 +0300

    add directive for change colors

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoFormComponent } from './todo/todo-form/todo-form.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { ColorDirective } from './shared/color.directive';
 
 @NgModule({
 	bootstrap: [AppComponent],
---
+++ src/app/shared/color.directive.js
@@ -0,0 +1,21 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({ 
+  selector:'[color]',
+  inputs: ['bgColor', 'colour:color']
+})
+
+export class ColorDirective {
+   
+  constructor(el: ElementRef) {
+    this.el = el;
+  }
+   
+  set colour(color){
+    this.el.nativeElement.style.color = color;
+  }
+   
+  set bgColor(color){
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
